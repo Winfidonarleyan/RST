e5c315cb1e76f7726ae63f7f7365834ca0f1073c
Winfidonarleyan <dowlandtop@yandex.com>
2022-06-09 15:26:37 +0700

    Init repo

---
+++ lost_number/solution.c
@@ -0,0 +1,20 @@
+/*
+ * solution.c
+ * Distributed under terms of the MIT license.
+ * Сложность: 7
+ */
+
+#include <stddef.h>
+
+/* Дан массив идущих подряд чисел от 1 до N в произвольном порядке.
+ * В случае пустого массива необходимо вернуть 0.
+ * Из массива убран один элемент.
+ * Найти пропущенное число.
+ */
+
+int lost_number(int* a, size_t size)
+{
+    // Your code here
+    return 0;
+}
+
---
+++ vowel_count/solution.c
@@ -0,0 +1,20 @@
+/*
+ * solution.c
+ * Distributed under terms of the MIT license.
+ * Сложность: 7
+ */
+
+#include <stddef.h>
+
+/*
+ * Функция get_count возвращает количество гласных в заданной строке.
+ * В качестве гласных рассматриваются только (a, e, i, o, u) но не (y).
+ * Входная строка будет состоять только из строчных букв и/или пробелов.
+ */
+
+size_t get_count(const char *s)
+{
+    // Your code here
+    return 0;
+}
+
