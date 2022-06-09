d828de7ff7dd9eeb2dd747eeb1aeed3189377184
Winfidonarleyan <dowlandtop@yandex.com>
2022-06-09 15:29:44 +0700

    feat(LN): impl `lost_number`

--- lost_number/solution.c
+++ lost_number/solution.c
@@ -5,6 +5,12 @@
  */
 
 #include <stddef.h>
+#include <stdlib.h>
+
+int SortCompare(const void* x1, const void* x2)
+{
+    return (*(int*)x1 - *(int*)x2);
+}
 
 /* Дан массив идущих подряд чисел от 1 до N в произвольном порядке.
  * В случае пустого массива необходимо вернуть 0.
@@ -14,7 +20,23 @@
 
 int lost_number(int* a, size_t size)
 {
-    // Your code here
-    return 0;
+    if (!a || !size)
+        return 0;
+
+    // Sorting by order
+    qsort(a, size, sizeof(int), SortCompare);
+
+    int findNumber = 0;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        if (a[i] != i + 1)
+        {
+            findNumber = i + 1;
+            break;
+        }
+    }
+    
+    return findNumber;
 }
 
