b763844056c8a44613aec01a27b758f7943d7727
Winfidonarleyan <dowlandtop@yandex.com>
2022-06-09 15:28:01 +0700

    feat(AP): impl `alphabet_position`

--- alphabet_position/solution.c
+++ alphabet_position/solution.c
@@ -5,6 +5,18 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int GetAlphabetPos(char ch)
+{
+    if (ch < 'a' || ch > 'z')
+        return 0;
+
+    return ch - 96;
+}
 
 /*
  * Необходимо, учитывая строку, заменить каждую букву ее позицией в алфавите.
@@ -20,7 +32,35 @@
 
 char *alphabet_position(char *text)
 {
-    // Your code here
+    int len = strlen(text);
+    // LOG_INFO("Len: {}", len);
+
+    if (len > 1024)
+        return text;
+
+    char newStr[1024] = "";
+    bool isFirst = true;
+
+    for (int i = 0; i < len; i++)
+    {
+        char ch = (char)tolower(text[i]);
+        int pos = GetAlphabetPos(ch);
+        if (!pos)
+            continue;
+
+        if (!isFirst)
+            strcat(newStr, " ");
+
+        if (isFirst)
+            isFirst = false;
+
+        char buff[5] = "";
+        snprintf(buff, 5, "%d", pos);
+        // itoa(pos, buff, 10);
+        strcat(newStr, buff);
+    }
+
+    text = newStr;
     return text;
 }
 
