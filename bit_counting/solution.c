5b4ebed202a50abdf6e2e5c41eb0dc50179f3b54
Winfidonarleyan <dowlandtop@yandex.com>
2022-06-09 15:29:10 +0700

    feat(BC): impl `countBits`

--- bit_counting/solution.c
+++ bit_counting/solution.c
@@ -15,9 +15,55 @@
  * двоичное представление 1234 это 10011010010, поэтому в этом случае функция должна вернуть 5.
  */
 
+// size_t countBits(unsigned value)
+// {
+//     char buff[100] = "";
+//     char* toInt = itoa(value, buff, 2);
+
+//     int len = strlen(toInt);
+//     size_t count = 0;
+
+//     for (int i = 0; i < len; i++)
+//     {
+//         if (toInt[i] == '1')
+//             count++;
+//     }
+
+//     return count;
+// }
+
 size_t countBits(unsigned value)
 {
-    // Your code here
-    return 0;
+    unsigned arr[50];
+    int i = 0;
+
+    for (; i < 50; i++)
+        arr[i] = 0;
+
+    i = 0;
+    while (value != 0)
+    {
+        if (value % 2 == 0)
+        {
+            arr[i] = 0;
+            value = value / 2;
+        }
+        else
+        {
+            arr[i] = 1;
+            value = value / 2;
+        }
+        i++;
+    }
+
+    i--;
+
+    size_t count = 0;
+
+    for (; i >= 0; i--)
+        if (arr[i] == 1)
+            count++;
+
+    return count;
 }
 
